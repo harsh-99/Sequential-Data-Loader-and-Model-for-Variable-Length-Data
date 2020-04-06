d5f58dd36fb7cc6f0836445a2ac3cc26a3ed1022
Harsh Maheshwari <harshmaheshwari135@gmail.com>
2020-04-06 16:04:30 +0530

    added comments

--- vocab.py
+++ vocab.py
@@ -3,17 +3,23 @@ import gensim
 from collections import Counter
 import json
 
+train_path = "./aclImdb/train"
+test_path = "./aclImdb/test"
+
 def build_vocab(path, min_word_count = 5):
 	pos_path = os.path.join(path, "pos")
 	neg_path = os.path.join(path, "neg")
+	#two folders, positive review and negative review
 	data = []
 	label = []
 	counter = Counter()
 	for file in os.listdir(pos_path):
 		f = open(os.path.join(pos_path, file))
 		line = f.read()
+		# we tokenize and preprocess the line using gensim
 		line = gensim.utils.simple_preprocess(line) 
 		data.append(line)
+		#used for building dictionary
 		counter.update(line)
 
 	for file in os.listdir(neg_path):
@@ -23,10 +29,12 @@ def build_vocab(path, min_word_count = 5):
 		data.append(line)
 		counter.update(line)
 
+	#initialise a dictionary or look up table
 	word2id = {}
 	word2id['<pad>'] = 0
 	word2id['<unk>'] = 1
 	# print(data[:1])
+	# include only those in dictionary which have occered more than min word count in the entire data.
 	words = [word for word, count in counter.items() if count>min_word_count]
 
 	for i, word in enumerate(words):
