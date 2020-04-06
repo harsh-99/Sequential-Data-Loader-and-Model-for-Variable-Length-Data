50b27618aef3aabe55c228e519d3164325118143
Harsh Maheshwari <harshmaheshwari135@gmail.com>
2020-04-06 16:25:30 +0530

    different function of reader and vocab:

--- vocab.py
+++ vocab.py
@@ -6,34 +6,33 @@ import json
 train_path = "./aclImdb/train"
 test_path = "./aclImdb/test"
 
-def build_vocab(path, min_word_count = 5):
+#simple function which read the data from directory and return data and label
+# you can make your own reader for other dataset.
+def reader(path):
 	pos_path = os.path.join(path, "pos")
 	neg_path = os.path.join(path, "neg")
-	#two folders, positive review and negative review
 	data = []
 	label = []
-	counter = Counter()
 	for file in os.listdir(pos_path):
 		f = open(os.path.join(pos_path, file))
-		line = f.read()
-		# we tokenize and preprocess the line using gensim
-		line = gensim.utils.simple_preprocess(line) 
-		data.append(line)
-		#used for building dictionary
-		counter.update(line)
-
+		data.append(f.read())
+		label.append(1)
 	for file in os.listdir(neg_path):
 		f = open(os.path.join(neg_path, file))
-		line = f.read()
-		line = gensim.utils.simple_preprocess(line) 
-		data.append(line)
-		counter.update(line)
+		data.append(f.read())
+		label.append(0)
+	# print(data[:1])
+	return data, label
 
+def build_vocab(data, min_word_count = 5):
+	counter = Counter()
+	for line in data:
+		l = gensim.utils.simple_preprocess(line)
+		counter.update(l)
 	#initialise a dictionary or look up table
 	word2id = {}
 	word2id['<pad>'] = 0
 	word2id['<unk>'] = 1
-	# print(data[:1])
 	# include only those in dictionary which have occered more than min word count in the entire data.
 	words = [word for word, count in counter.items() if count>min_word_count]
 
@@ -44,4 +43,6 @@ def build_vocab(path, min_word_count = 5):
 		json.dump(word2id, f)
 	return word2id
 
-word2id = build_vocab(train_path)
+data, label = reader(train_path)
+word2id = build_vocab(data)
+print("Dictionary Formed and saved. The length of dictionary is-: ", len(word2id))
